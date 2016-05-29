c31a95b87d416ba9304599ba15fee56d0205cbf1
2785888 <2785888@mail.ru>
2016-05-29 17:38:19 +0000

    cart

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,46 +1,97 @@
-	// var x=12;
-	// var y=5;
-	// var result=x+y;
-	// alert(result);
+var products = [
+	{
+		"title": "iPhone",
+		"price": "300"
+	},
+	{
+		"title": "Android",
+		"price": "250"
+	},
+	{
+		"title": "Phone",
+		"price": "200"
+	}	
+]
 
-	// var message="Hello world!";
-	// var hello;
-	// hello=message;
-	// console.log(message);
-	// console.log(hello);
+$(function () {
+	$(".in_cart").click(function(){
+		console.dir($(this).attr("data-id"));
 
-// var admin;
-// var name;
-// name="Василий";
-// admin=name;
-// console.log(admin);
-// alert(admin);
+		var id = $(this).attr("data-id");
 
-// var result=prompt("Ты серьезно?", "yes");
-// var isAdmin=confirm("Вы - администратор?");
+		if ($("#cart").html() == "Корзина пуста") {
+			$("#cart").html(render(products[id]))
+		} else {
+			$("#cart").append(render(products[id]))
+		}
+	
+	})
 
-// console.log(result, isAdmin); 
 
-function iter() {
-	var elements = document.getElementsByTagName("li");
+	$("body").on("click", ".product-rm", function() {
+		$(this).parent().remove()
 
-	for (var i=0; i<elements.length; i++) {
-		console.log(elements[i]);
-	}
+		if ($(".product-rm").length == 0) {
+			$("#cart").html("Корзина пуста")
+		}
+	});
+})
 
-	// console.log(document.getElementsByID(ul.menu))
+function render (product) {
+	var template = "<div>" + product.title + ", Цена " + product.price + " <button class = 'product-rm'>Удалить</button> </div>"
+
+	return template
 }
 
-function more() {
-	var div = document.createElement('div');
-	div.className = "success";
-	div.innerHTML = "Вы добавили товар в корзину!";
 
-	// document.body.appendChild(div);
 
-	var p = document.querySelector("p");
 
-	document.body.insertBefore(div, p)
 
-// 	document.body.innerHTML = "<div>Вы добавилитовар в корзину!</div>"
-}
+
+
+// 	// var x=12;
+// 	// var y=5;
+// 	// var result=x+y;
+// 	// alert(result);
+
+// 	// var message="Hello world!";
+// 	// var hello;
+// 	// hello=message;
+// 	// console.log(message);
+// 	// console.log(hello);
+
+// // var admin;
+// // var name;
+// // name="Василий";
+// // admin=name;
+// // console.log(admin);
+// // alert(admin);
+
+// // var result=prompt("Ты серьезно?", "yes");
+// // var isAdmin=confirm("Вы - администратор?");
+
+// // console.log(result, isAdmin); 
+
+// function iter() {
+// 	var elements = document.getElementsByTagName("li");
+
+// 	for (var i=0; i<elements.length; i++) {
+// 		console.log(elements[i]);
+// 	}
+
+// 	// console.log(document.getElementsByID(ul.menu))
+// }
+
+// function more() {
+// 	var div = document.createElement('div');
+// 	div.className = "success";
+// 	div.innerHTML = "Вы добавили товар в корзину!";
+
+// 	// document.body.appendChild(div);
+
+// 	var p = document.querySelector("p");
+
+// 	document.body.insertBefore(div, p)
+
+// // 	document.body.innerHTML = "<div>Вы добавилитовар в корзину!</div>"
+// }
